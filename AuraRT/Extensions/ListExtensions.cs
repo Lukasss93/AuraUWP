1138cbd4d74b808669d7bdb4ba39d44ec0155c8f
Luca Patera <email@lucapatera.it>
2016-04-11 22:33:29 +0200

    Added ListExtensions

---
+++ AuraRT/Extensions/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraRT.Extensions
+{
+    public static class ListExtensions
+    {
+        public static T GetRandomItem<T>(this List<T> list)
+        {
+            Random rand = new Random();
+            return list[rand.Next(0, list.Count)];
+        }
+    }
+}
